6662da328fb17a6f56105612db82cada98cfbeab
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2025-03-18 19:31:24 +0100

    improv: run scan every 2 hours

--- services/discord_service.py
+++ services/discord_service.py
@@ -33,7 +33,7 @@ class MyDiscordClient(discord.Client):
         logging.debug("""Logged in as %s (ID: %s)""", self.user, self.user.id)
         logging.debug("------")
 
-    @tasks.loop(hours=6)  # task runs every 6 hour
+    @tasks.loop(hours=2)  # task runs every 2 hours
     async def my_background_task(self):
         """
         Background task that runs every day.
