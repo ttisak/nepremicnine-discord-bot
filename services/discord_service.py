c58752bdb2d359c1ad79bf5c176af3360ca8d329
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2025-04-03 21:10:10 +0200

    improv: handle spider crash

--- services/discord_service.py
+++ services/discord_service.py
@@ -3,11 +3,10 @@
 Module that contains discord bot logic.
 """
 
-import logging
 
 import discord
 from discord.ext import tasks
-
+from logger.logger import logger
 from database.database_manager import DatabaseManager
 from spider.spider import run_spider
 
@@ -30,8 +29,8 @@ class MyDiscordClient(discord.Client):
         Called when the bot is ready.
         :return:
         """
-        logging.debug("""Logged in as %s (ID: %s)""", self.user, self.user.id)
-        logging.debug("------")
+        logger.debug("""Logged in as %s (ID: %s)""", self.user, self.user.id)
+        logger.debug("------")
 
     @tasks.loop(hours=2)  # task runs every 2 hours
     async def my_background_task(self):
@@ -40,73 +39,80 @@ class MyDiscordClient(discord.Client):
         :return:
         """
 
-        logging.info("Scan started.")
+        logger.info("Scan started.")
 
         # Setup database manager.
         database_manager = DatabaseManager(
             url="sqlite+aiosqlite:///" + self.database_path
         )
 
-        # Run the spider.
-        channel_listings, error = await run_spider(database_manager=database_manager)
+        try:
+            # Run the spider.
+            channel_listings, error = await run_spider(
+                database_manager=database_manager
+            )
 
-        for channel_id, listings in channel_listings.items():
-            logging.debug("Sending listings to channel %s.", channel_id)
+            for channel_id, listings in channel_listings.items():
+                logger.debug("Sending listings to channel %s.", channel_id)
 
-            channel = self.get_channel(int(channel_id))  # channel ID goes here
-            # await channel.send("Hello, world!")
+                channel = self.get_channel(int(channel_id))  # channel ID goes here
+                # await channel.send("Hello, world!")
 
-            logging.debug("Found %s new listings.", len(listings))
+                logger.debug("Found %s new listings.", len(listings))
 
-            await channel.send(f"Found {len(listings)} new listings.")
+                await channel.send(f"Found {len(listings)} new listings.")
 
-            for listing in listings:
-                title, image_url, description, prices, size, year, floor, url = listing
-
-                logging.debug("Listing: %s", listing)
-
-                embed = discord.Embed(
-                    title=title,
-                    url=url,
-                    description=description,
-                    color=discord.Color.blue(),
-                )
-                if image_url:
-                    embed.set_image(url=image_url)
-                embed.add_field(
-                    name="**Cena**",
-                    value=f"{prices[0]:.2f} €",
-                    inline=True,
-                )
-                embed.add_field(
-                    name="**Velikost**",
-                    value=f"{size:.2f} m²",
-                    inline=True,
-                )
-                embed.add_field(
-                    name="**Zgrajeno leta**",
-                    value=year,
-                    inline=True,
-                )
-                embed.add_field(
-                    name="**Nadstropje**",
-                    value=floor,
-                    inline=True,
-                )
-
-                if len(prices) > 1:
-                    embed.add_field(
-                        name="**Prejšnje cene**",
-                        value=", ".join(f"{price:.2f} €" for price in prices[1:]),
-                        inline=False,
+                for listing in listings:
+                    title, image_url, description, prices, size, year, floor, url = (
+                        listing
                     )
 
-                await channel.send(embed=embed)
+                    logger.debug("Listing: %s", listing)
 
-            if error:
-                await channel.send("An error occurred while scanning the website.")
+                    embed = discord.Embed(
+                        title=title,
+                        url=url,
+                        description=description,
+                        color=discord.Color.blue(),
+                    )
+                    if image_url:
+                        embed.set_image(url=image_url)
+                    embed.add_field(
+                        name="**Cena**",
+                        value=f"{prices[0]:.2f} €",
+                        inline=True,
+                    )
+                    embed.add_field(
+                        name="**Velikost**",
+                        value=f"{size:.2f} m²",
+                        inline=True,
+                    )
+                    embed.add_field(
+                        name="**Zgrajeno leta**",
+                        value=year,
+                        inline=True,
+                    )
+                    embed.add_field(
+                        name="**Nadstropje**",
+                        value=floor,
+                        inline=True,
+                    )
 
-        logging.info("Scan finished.")
+                    if len(prices) > 1:
+                        embed.add_field(
+                            name="**Prejšnje cene**",
+                            value=", ".join(f"{price:.2f} €" for price in prices[1:]),
+                            inline=False,
+                        )
+
+                    await channel.send(embed=embed)
+
+                if error:
+                    await channel.send("An error occurred while scanning the website.")
+
+            logger.info("Scan finished.")
+        except Exception as e:  # pylint: disable=broad-except
+            logger.warning("Spider crashed with error: %s", e)
 
     @my_background_task.before_loop
     async def before_my_task(self):
